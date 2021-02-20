98463c9b309a92533b3fb0967fed1d06de6c3241
SeongBeom27 <lgh121546@naver.com>
2021-02-20 15:17:45 +0900

    Node Express 1. 홈, 상세 페이지 구현

--- main.js
+++ main.js
@@ -2,14 +2,53 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-// Get method : Routing
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const template = require('./lib/template.js');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+//Get method : Routing
 
 // -> 길을 따라 갈 때 적당한 곳으로 가게 방향을 잡아주는 역할
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    fs.readdir('./data', function(error, filelist) {
+        let title = 'Welcome';
+        let description = 'Hello, Node.js';
+        let list = template.list(filelist);
+        let html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+        );
+        res.send(html);
+    });
 })
 
+app.get('/page/:pageId', function(req, res) {
+    fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(req.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            var title = req.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags: ['h1']
+            });
+            var list = template.list(filelist);
+            var html = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                ` <a href="/create">create</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            res.send(html);
+        });
+    });
+    // return이 있어도되고 없어도 된다.
+});
 /**
  * ==
  *  app.get('/', function(req, res){
