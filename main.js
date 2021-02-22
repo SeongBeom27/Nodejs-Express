09b69779888e8ca9b2db15db15ef6f6f0e4226af
SeongBeom27 <lgh121546@naver.com>
2021-02-22 15:07:57 +0900

    Node Express 4. 페이지 삭제 구현

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ app.get('/page/:pageId', function(req, res) {
                 `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                 ` <a href="/create">create</a>
                 <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
+                <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
@@ -127,10 +127,24 @@ app.post('/update_process', function(req, res) {
         var description = post.description;
         fs.rename(`data/${id}`, `data/${title}`, function(error) {
             fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                res.writeHead(302, { Location: `/?id=${title}` });
-                res.end();
+                res.redirect(`/?id=${title}`);
             })
         });
+    });
+});
+
+app.post('/delete_process', function(req, res) {
+    var body = '';
+    req.on('data', function(data) {
+        body = body + data;
+    });
+    req.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error) {
+            res.redirect('/');
+        })
     });
 });
 
