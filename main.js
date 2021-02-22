14d8e4b71d826c4822d03101c4ed4876efbd44dd
SeongBeom27 <lgh121546@naver.com>
2021-02-22 14:58:32 +0900

    Node Express 3. 페이지 수정 구현

--- main.js
+++ main.js
@@ -86,6 +86,54 @@ app.post('/create_process', function(req, res) {
         })
     });
 });
+
+app.get('/update', function(req, res) {
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+    fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            var title = queryData.id;
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
+                `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
+            res.send(html);
+        });
+    });
+})
+
+app.post('/update_process', function(req, res) {
+    var body = '';
+    req.on('data', function(data) {
+        body = body + data;
+    });
+    req.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) {
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                res.writeHead(302, { Location: `/?id=${title}` });
+                res.end();
+            })
+        });
+    });
+});
+
 /**
  * ==
  *  app.get('/', function(req, res){
