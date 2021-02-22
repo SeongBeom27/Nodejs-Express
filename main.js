96b626a8f36e60302685d50544a27d14b9b743b8
SeongBeom27 <lgh121546@naver.com>
2021-02-22 14:49:42 +0900

    Node Express 2. 페이지 생성 구현

--- main.js
+++ main.js
@@ -50,6 +50,42 @@ app.get('/page/:pageId', function(req, res) {
     });
     // return이 있어도되고 없어도 된다.
 });
+
+app.get('/create', function(req, res) {
+    fs.readdir('./data', function(error, filelist) {
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
+              <form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                  <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+            `, '');
+        res.send(html);
+    });
+});
+
+// post 방식으로 데이터 받는 경우
+app.post('/create_process', function(req, res) {
+    var body = '';
+    req.on('data', function(data) {
+        body = body + data;
+    });
+    req.on('end', function() {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+            res.writeHead(302, { Location: `/?id=${title}` });
+            res.end();
+        })
+    });
+});
 /**
  * ==
  *  app.get('/', function(req, res){
