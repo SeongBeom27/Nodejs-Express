3ad6f2ba9016ee2bfdd92eec489d46974a599d5c
SeongBeom27 <lgh121546@naver.com>
2021-04-10 09:49:39 +0900

    Node Express 33. Express-Session Session Store 원리 및 사용법

--- express_session.js
+++ express_session.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
-
+var FileStore = require('session-file-store')(session)
 var app = express()
 
 app.use(session({
@@ -17,7 +17,13 @@ app.use(session({
      * true     : session이 store에 저장되기 전에 초기화 되지 않은 상태로 만들어서 저장한다.
      * false    : session이 store에 저장되기 전에 초기화 된 상태로 만들어서 저장한다.
      */
-    saveUninitialized: true
+    saveUninitialized: true,
+    /**
+     * 사용자가 session 아이디를 전달하면
+     * session store에서 아이디에 대응되는 적당한 파일에 데이터를 저장하고
+     * request session 객체에 적당한 프로퍼티를 추가해준다.
+     */
+    store: new FileStore()
 }))
 
 app.get('/', function(req, res, next) {
